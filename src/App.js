07d0b5f4aca02388ef08cd342aebc14bb7b8dc80
danil <powerdan777@gmail.com>
2021-01-22 22:22:23 +0300

    Third lesson of final project. Elements in React. Delete all files in folder: src except App.js and index.js, all files in folder: public except index.html and some file in our project folder (name: todo). Make element in App.js, export it to the index.js and render it with ReactDom.render

--- src/App.js
+++ src/App.js
@@ -1,52 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import { count } from './number.js';
-import { length } from './number.js';
+import React from 'react';
 
+const element = (<div>
+  Hello World!
+  <a href="">go to</a>
+</div>);
 
-const a = 'Thats for show variable'
-const b = 42
-const c = 42 / 2
-const flag = true
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p style = {{
-          color: "green"
-        }}>
-          Hello World!
-        </p>
-        <p>
-          { a + " "}          
-          { b + " "}
-          { c + " "}
-          { flag && 'Flag is true' + ' '}
-          { flag ? "and thats true!" : "Oh, no... False"}
-          { undefined }
-          { null }
-          { false }
-          { true }
-        </p>
-        <p>
-          { 'count' + '*' + 'length' + '=' + count*length }
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
