92fa2cc57a146c18baa7a9750c8cde8c58ca2126
danil <powerdan777@gmail.com>
2021-01-23 00:36:52 +0300

    latest

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ItemList from '../ItemList/ItemList';
+
+const App = () => (
+    <div>
+        <h1>Важные дела:</h1>
+        <ItemList />
+    </div>
+);
+
+export default App;
+
---
+++ src/components/Item/Item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Item = () => (
+    <span>Очень Важное дело!</span>
+);
+
+export default Item;
