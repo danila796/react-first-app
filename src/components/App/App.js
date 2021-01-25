4bc7f49f6fad62e91e2160f1b48a9c8dda639cba
danil <powerdan777@gmail.com>
2021-01-25 22:54:35 +0300

    Sixth lesson of final project. Props. Added some new components like Footer.js and InputItem.js.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
+import Footer from '../Footer/Footer';
 
+const todoItem = 'Написать новое приложение';
+const count = 3;
 const App = () => (
     <div>
         <h1>Важные дела:</h1>
-        <ItemList />
+        <InputItem />
+        <ItemList todoItem = { todoItem } />
+        <Footer count = { count } />
     </div>
 );
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = ({ count }) => (
+    <div>Осталось ровно столько дел: { count }</div>
+);
+
+export default Footer;
