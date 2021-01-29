1623d93e2704c41c17b6c0317a53207f68b556b7
danil <powerdan777@gmail.com>
2021-01-29 23:50:24 +0300

    Tenth lesson of final project. Classnames. Install classnames with npm and import it. Also create new Item.module.css file and import it to Item (classnames import too). Write classnames for span which change style of items because of prop isDone, which we import in items as second keys in App.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import classnames from 'classnames';
+import styles from './Item.module.css';
 
-const Item = ({ value }) => (
-    <span>{ value}</span>
+const Item = ({ value, isDone }) => (
+    <span className={
+        classnames({
+            [styles.item]: true,
+            [styles.done]: isDone
+        })  
+    }>
+        { value }
+    </span>
 );
 
 export default Item;
