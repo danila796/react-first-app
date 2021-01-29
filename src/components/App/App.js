c477564889c2276de8d001ac53fc00a2803b61c4
danil <powerdan777@gmail.com>
2021-01-29 22:51:12 +0300

    Ninth lesson of final project. Css modules. Rename App.css to App.module.css, import it like 'styles' from App.module.css and get css styles from object 'styles' and keys in it like: 'styles.wrap' or 'styles.title'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
-import './App.css';
+import styles from './App.module.css';
 
 const count = 3;
 const App = () => {
@@ -20,8 +20,8 @@ const App = () => {
     ]
 
     return (
-        <div className="wrap">
-            <h1 className="wrap__title">Важные дела:</h1>
+        <div className={styles.wrap}>
+            <h1 className={styles.title}>Важные дела:</h1>
             <InputItem />
             <ItemList items = { items } />
             <Footer count = { count }/>
