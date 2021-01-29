e9ff678d866d42c7a675b6ae57765b38fb10db41
danil <powerdan777@gmail.com>
2021-01-30 00:08:28 +0300

    Eleventh lesson of final project. Complete packages of components and Material UI. Setup material-ui with npm and add component from it to InputItem

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const InputItem = () => (
-    <input></input>
+    <div>
+        <TextField
+          label="Добавить новое задание"
+          id="margin-dense"
+          margin="dense"
+        />
+    </div>
 );
 
 export default InputItem;
